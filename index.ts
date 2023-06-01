d31c57113f7bcb254812e57676f55ff3483d6f51
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-06-01 23:25:19 +0530

    passing an object on the request

--- index.ts
+++ index.ts
@@ -1,15 +1,35 @@
-import express,{Application,Request,Response} from 'express'
-import cors from "cors"
-import {Configuration,OpenAIApi} from "openai"
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
+const PORT: number = 8000;
 
-const PORT: number = 8000
+const app: Application = express();
 
-const app: Application = express()
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+const API_KEY: string = "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";
 
-const API_KEY:string='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'
+const configuration = new Configuration({
+  apiKey: API_KEY,
+});
 
+const openai = new OpenAIApi(configuration);
 
+app.post("/completions", async (req: Request, res: Response) => {
+  try {
+    const completion = await openai.createChatCompletion({
+      model:"gpt-4",
+      messages: [{
+        role:"user",
+        content:"Create a SQL requeste to" + req.body.message
+      }],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port:  ${PORT}`));
